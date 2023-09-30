9569f6fe7f784e62fc4731fd8ed380336e517c2f
caldardn <caldardn@gmail.com>
2023-09-30 01:37:59 -0400

    Adding updated HTML code and JS code that diplays the current weather and the next 5 days

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,11 @@ let userInputEl = document.querySelector('#user-input')
 let locationEl = document.querySelector('#location')
 
 
+
+
+
+
+
 userInputEl.addEventListener('submit', function(e){
     e.preventDefault()
     let location = locationEl.value
@@ -19,8 +24,47 @@ userInputEl.addEventListener('submit', function(e){
     })
     .then(function(data){
         console.log(data)
+        document.getElementById('city').textContent = "City: " + data.city.name
+        document.getElementById('currentDate').textContent = "date" + data.list[0].dt_txt
+        document.getElementById('currentTemp').textContent = "Temperature: " + data.list[0].main.temp + "\xB0" + "F"
+        document.getElementById('currentDisc').textContent = "Discription: " + data.list[0].weather[0].description
+        document.getElementById('currentWind').textContent = "Wind Speed : " + data.list[0].wind.speed + " MPH"
+        document.getElementById('currentHumidity').textContent = "Humidity: " + data.list[0].main.humidity + "%"
+        console.log(data.list[0].main)
+
+        document.getElementById('date1').textContent = "date" + data.list[9].dt_txt
+        document.getElementById('temperature1').textContent = "Temperature: " + data.list[9].main.temp + "\xB0" + "F"
+        document.getElementById('discription1').textContent = "Discription: " + data.list[9].weather[0].description
+        document.getElementById('wind1').textContent = "Wind Speed : " + data.list[9].wind.speed + " MPH"
+        document.getElementById('humidity1').textContent = "Humidity: " + data.list[9].main.humidity + "%"
+
+        document.getElementById('date2').textContent = "date" + data.list[17].dt_txt
+        document.getElementById('temperature2').textContent = "Temperature: " + data.list[17].main.temp + "\xB0" + "F"
+        document.getElementById('discription2').textContent = "Discription: " + data.list[17].weather[0].description
+        document.getElementById('wind2').textContent = "Wind Speed : " + data.list[17].wind.speed + " MPH"
+        document.getElementById('humidity2').textContent = "Humidity: " + data.list[17].main.humidity + "%"
+
+        document.getElementById('date3').textContent = "date" + data.list[25].dt_txt
+        document.getElementById('temperature3').textContent = "Temperature: " + data.list[25].main.temp + "\xB0" + "F"
+        document.getElementById('discription3').textContent = "Discription: " + data.list[25].weather[0].description
+        document.getElementById('wind3').textContent = "Wind Speed : " + data.list[25].wind.speed + " MPH"
+        document.getElementById('humidity3').textContent = "Humidity: " + data.list[25].main.humidity + "%"
+
+        document.getElementById('date4').textContent = "date" + data.list[33].dt_txt
+        document.getElementById('temperature4').textContent = "Temperature: " + data.list[33].main.temp + "\xB0" + "F"
+        document.getElementById('discription4').textContent = "Discription: " + data.list[33].weather[0].description
+        document.getElementById('wind4').textContent = "Wind Speed : " + data.list[33].wind.speed + " MPH"
+        document.getElementById('humidity4').textContent = "Humidity: " + data.list[33].main.humidity + "%"
+
+        document.getElementById('date5').textContent = "date" + data.list[39].dt_txt
+        document.getElementById('temperature5').textContent = "Temperature: " + data.list[39].main.temp + "\xB0" + "F"
+        document.getElementById('discription5').textContent = "Discription: " + data.list[39].weather[0].description
+        document.getElementById('wind5').textContent = "Wind Speed : " + data.list[39].wind.speed + " MPH"
+        document.getElementById('humidity5').textContent = "Humidity: " + data.list[39].main.humidity + "%"
     })
-})
+ })
+   
+
 
 
 
