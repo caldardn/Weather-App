b61e3f0254529782301b928f4d31708528a57169
caldardn <caldardn@gmail.com>
2023-09-29 23:25:39 -0400

    Adding Inital code and API fetch request

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+const key = '126d2bb719bd2d3f71661c8d719d7efd'
+let userInputEl = document.querySelector('#user-input')
+let locationEl = document.querySelector('#location')
+
+
+userInputEl.addEventListener('click', function(e){
+    e.preventDefault()
+    let location = locationEl.value
+   fetch(
+    'https://api.openweathermap.org/data/2.5/forecast?q='+location+'&appid='+key+'&units=imperial',
+    {
+        method: "GET",
+        credentials: "same-origin",
+        redirect: "follow",
+    }
+    )
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+    })
+})
+
+
+
